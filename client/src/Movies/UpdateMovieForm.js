2f4d19ec6e077d34f149217673549b00dc5dd978
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-05-20 13:38:16 -0500

    First push to my branch

---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const UpdateMovieForm = () => {
+    return (
+        <form>
+            <input />
+        </form>    
+
+    )
+}
+
+export default UpdateMovieForm;
